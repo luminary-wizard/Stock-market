278888a5bab30908f53ec847b38c648dc52fedba
luminary-wizard <trazquip@gmail.com>
2025-04-30 12:37:49 +0530

    Update minorproject2.py

--- minorproject2.py
+++ minorproject2.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 import streamlit as st
 from datetime import datetime
 import time
